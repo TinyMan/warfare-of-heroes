35ddd272a630d7ae73d1ba07c6536db890f2166d
Morgan_Feurte <morgan.feurte@gmail.com>
2015-03-16 15:20:57 +0100

    Character + Classes (Archer/Mage/Knight)

--- main.cpp
+++ main.cpp
@@ -1,30 +1,10 @@
 #include <SDL2/SDL.h>
-#include <iostream>
-#include "ServiceLocator.h"
-
-void myCallback(int a, TimeService* ts)
-{
-	cout << SDL_GetTicks() << "\ta = " << a  << endl;
-}
-
+#include "Character.h"
 
 int main(int argc, char* argv[])
 {
-	SDL_Init(SDL_INIT_EVERYTHING);
-	
-	cout << "Starting @ " << SDL_GetTicks() << endl;
-	ServiceLocator::provide(new TimeService());
-	ServiceLocator::provide(new LogService("log.txt"));
-	
-	ServiceLocator::getTimeService()->setTimeout(1000, Callback(myCallback, 1, ServiceLocator::getTimeService()));
-	while (SDL_GetTicks() < 5000)
-	{
-		SDL_Delay(100);
-		ServiceLocator::getTimeService()->update();
-	}
-	cout << "Ending @ " << SDL_GetTicks() << endl;
+	Archer a();
 
-	system("pause");
-	SDL_Quit();
+
 	return 0;
 }
