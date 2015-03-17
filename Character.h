af28de51f309576b85ce168c17080e4ddd6d531e
Morgan_Feurte <morgan.feurte@gmail.com>
2015-03-17 13:27:57 +0100

    CharacterClass

---
+++ Character.h
@@ -0,0 +1,36 @@
+#pragma once
+#include <iostream>
+
+using namespace std;
+
+class Character
+{
+protected:
+	string _name;
+	int _hitPoints;
+	int _movementPoints;
+	int _capacityPoints;
+	// Cell positionOnGrid;
+
+public:
+	Character(string name="NoName");
+	virtual ~Character();
+
+	// Setters :
+	void lowerHitPoint(int amount);
+	void removeMovementPoint(int amount=1);
+	void removeCapaciyPoint(int amount=1);
+
+	// Getters :
+	int getHP();
+	int getMP();
+	int getCP();
+
+	// Attacks :
+	bool basicAttack(Character & c);
+
+	// Movements :
+	bool movement(); // Français : on met en paramètre la case et on vérifie si on peut se déplacer 
+	// Il faut créer une méthode de pathfinding qui permettra de se déplacer de plus d'une case à la fois.
+};
+
