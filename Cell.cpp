53a141d52c259b07b138b5000819880b00e9f11e
Morgan_Feurte <morgan.feurte@gmail.com>
2015-03-18 12:30:31 +0100

    Cell_someFunctions

--- Cell.cpp
+++ Cell.cpp
@@ -16,3 +16,29 @@ void Cell::setType(_STATE t)
 {
 	_cellType = t;
 }
+
+int Cell::getDistance(const Cell & c) const
+// Returns the distance between the object and the cell given as parameter
+{
+	int X1 = c.getPosX();
+	int Y1 = c.getPosY();
+	int X2 = this.getPosX();
+	int Y2 = this.getPosY();
+
+	int temp,distance;
+	if (X1 > X2)
+	{
+		temp = X2;
+		X2 = X1;
+		X1 = temp;
+	}
+	if (Y1 > Y2)
+	{
+		temp = Y2;
+		Y2 = Y1;
+		Y1 = temp;
+	}
+
+	distance = (Y2 - Y1) + (X2 - X1);
+	return distance;
+}
