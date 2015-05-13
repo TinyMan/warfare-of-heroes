3d0dcbdf983b02564c5f2ca1ccca46113583cd46
Raphael Uzan <raph.a.uzan@gmail.com>
2015-05-14 00:44:48 +0200

    Clean the main

--- main.cpp
+++ main.cpp
@@ -1,24 +1,7 @@
-#include <SDL2/SDL.h>
-#include <typeinfo>
 #include <iostream>
 #include "Game.h"
 #include "ServiceLocator.h"
-#include "Grid.h"
-#include "Mage.h"
-#include "Knight.h"
-#include "Archer.h"
-#include "Spell.h"
-#include "SquareAoE.h"
-#include "DiamondAoE.h"
-#include "DamageEffect.h"
-#include "LineAoE.h"
-#include "Button.h"
-#include "Panel.h"
-#include "Font.h"
-#include "Label.h"
-#include "Line.h"
-#include "Polygon.h"
-#include "Geometry.h"
+
 using namespace std;
 
 int main(int argc, char* argv[])
