2881d82351a202ce93ca48aa09e02df5a4ded42a
Raphael Uzan <raph.a.uzan@gmail.com>
2015-06-23 15:21:36 +0200

    Volley now display only 1 volley for all the AoE

--- VolleyOctopus.cpp
+++ VolleyOctopus.cpp
@@ -5,7 +5,7 @@ VolleyOctopus::VolleyOctopus(Panel* container, GridOctopus * gridO, Spell* s, Ch
 {
 
 	Texture t = (*ServiceLocator::getTextureManager())["VoleeFlechesFrame1"];
-	Point d = gridO->getCellDimensions();
+	Point d = gridO->getCellDimensions() * 5;
 	// compute ratio
 	double ratiox = (double)d.x / t.getWidth();
 	double ratioy = (double)d.y / t.getHeight();
@@ -64,13 +64,13 @@ void VolleyOctopus::internalRender(SDL_Renderer* r, bool force)
 		{
 			int width = int(_tex.getWidth() * ratio), height = int(_tex.getHeight() * ratio);
 
-			for (Cell* c : _target->getCells())
-			{
-				Point center = _grid->getCellCenter(c->getNumber());
+			//for (Cell* c : _target->getCells())
+			//{
+				Point center = _grid->getCellCenter(_target->getCell()->getNumber());
 				SDL_Rect dst = { int(center.x - width / 2), int(center.y - height / 2), width, height };
 
 				SDL_RenderCopy(r, _tex, nullptr, &dst);
-			}
+			//}
 		}
 	}
 }
